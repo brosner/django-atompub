23efd7ef0c0fd6c76be7a9f7c20aab1150731df3
Brian Rosner <brosner@gmail.com>
2009-06-30 10:08:39 -0600

    hopefully a working setup.py file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "django-atompub",
+    version = "0.1.0dev",
+    author = "James Tauber",
+    url = "http://code.google.com/p/django-atompub/",
+    packages = find_packages(),
+    zip_safe = False,
+)
